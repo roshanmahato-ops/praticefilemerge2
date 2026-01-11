76b159cf2b4b454282e1a83da15abdad273cc490
roshanmahato-ops <roshabmahato@gmail.com>
2026-01-11 08:27:27 +0530

    Add M.B. in main branch

---
+++ mergepratice.java
@@ -0,0 +1,8 @@
+package pratcefiel2;
+
+// PRATICING CONFLICT MERGE
+public class mergepratice {
+    public static void main(String[] args) {
+        System.out.println("  ADD 'MAIN BRANCH' IN MAIN BRANCH   ");
+    }
+}
