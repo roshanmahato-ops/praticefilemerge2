503b68df034f1955434e57e6d81240f3acf35fc5
roshanmahato-ops <roshabmahato@gmail.com>
2026-01-11 08:51:00 +0530

    add button in main

--- mergepratice.java
+++ mergepratice.java
@@ -5,5 +5,6 @@ public class mergepratice {
     public static void main(String[] args) {
         System.out.println("  ADD 'MAIN BRANCH' IN MAIN BRANCH   ");
         System.out.println("LYING IN MAIN FUNCTION ");
+        System.out.println(" 'MAIN' ADD BUTTON ");
     }
 }
